9219b002f74e4ce441a19f5e6a50adbeaff243a2
vlad <VladDaImpaler@gmail.com>
2020-10-14 23:56:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 let addToy = false;
+const toyCollection = document.getElementById("toy-collection");
+const toyName = document.querySelectorAll("input.input-text")[0];
+const toyUrl = document.querySelectorAll("input.input-text")[1];
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +15,44 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let formData = {
+      [name]: toyName.value,
+      [image]: toyUrl.value,
+      //[likes]: "0"
+    };
+    createCard(formData);
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    }
+    e.target.reset();
+  });
 });
+
+function createCard(object){
+  const div = document.createElement("div");
+    div.className = "card";
+  const nameNode = document.createElement("h2");
+    nameNode.textContent = object.name;
+  const img = document.createElement("img");
+    img.className = "toy-avatar";
+    img.src = object.image;
+  const likePart = document.createElement("p");
+    likePart.textContent = `${object.likes} likes`;
+  const likeButton = document.createElement("BUTTON");
+    likeButton.className = "like-btn";
+    likeButton.textContent = "Like <3";
+    div.appendChild(nameNode);
+    div.appendChild(img);
+    div.appendChild(likePart);
+    div.appendChild(likeButton);
+
+    toyCollection.appendChild(div);
+}
